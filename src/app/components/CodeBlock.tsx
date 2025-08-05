911b7f89b85bf9178fd1138b1b266fa275b73757
Calum Macdonald <calmacx@gmail.com>
2025-08-05 09:36:03 +0100

    revert syntax

--- src/app/components/CodeBlock.tsx
+++ src/app/components/CodeBlock.tsx
@@ -1,10 +1,13 @@
 import { PrismAsyncLight as SyntaxHighlighter } from "react-syntax-highlighter";
-//import { materialLight } from "react-syntax-highlighter/dist/styles/prism";
-//style={materialLight}
+import { materialLight } from "react-syntax-highlighter/dist/styles/prism";
 
 const CodeBlock = ({ code }: { code: unknown }) => {
   const codeString = JSON.stringify(code, null, 2);
-  return <SyntaxHighlighter language="json">{codeString}</SyntaxHighlighter>;
+  return (
+    <SyntaxHighlighter language="json" style={materialLight}>
+      {codeString}
+    </SyntaxHighlighter>
+  );
 };
 
 export default CodeBlock;
