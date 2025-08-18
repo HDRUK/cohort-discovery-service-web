ac41f867a5e89f7e4639509124b058077bc3de53
Calum Macdonald <calmacx@gmail.com>
2025-08-18 10:33:51 +0100

    missing files

---
+++ src/app/config/routes.ts
@@ -0,0 +1,13 @@
+const dashboardPath = (subpath: string = "") =>
+  `/dashboard${subpath ? `/${subpath}` : ""}` as const;
+
+export const routes = {
+  dashboard: dashboardPath(),
+  dashboardNewQuery: dashboardPath("new-query"),
+  dashboardNewQueryWithPid: (pid: string) => ({
+    pathname: dashboardPath("new-query"),
+    query: { query: pid },
+  }),
+  dashboardHistory: dashboardPath("history"),
+  dashboardCollections: dashboardPath("collections"),
+};
