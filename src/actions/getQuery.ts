f8a798d817d2029141ed11052485c3b7a0da8bbd
Calum Macdonald <calmacx@gmail.com>
2025-09-09 11:16:50 +0100

    removing temp files

--- src/actions/getQuery.ts
+++ src/actions/getQuery.ts
@@ -4,9 +4,11 @@ import { apiGet } from "../lib/apiClient";
 import { API_ROUTES } from "../lib/apiRoutes";
 import { Query, ApiResponse } from "../types/api";
 
-const getQuery = async (queryId: string): Promise<ApiResponse<Query>> => {
-  return await apiGet<ApiResponse<Query>>(API_ROUTES.getQuery(queryId), {
-    next: { tags: ["query", queryId] },
+const getQuery = async (pid: string): Promise<ApiResponse<Query>> => {
+  return await apiGet<ApiResponse<Query>>(API_ROUTES.getQuery(pid), {
+    next: {
+      tags: ["query", pid],
+    },
   });
 };
 
