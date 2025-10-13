9b60ee7d0abed1d7bc7210a94300acad90c1d9dd
Calum Macdonald <calmacx@gmail.com>
2025-10-13 13:42:48 +0100

    fix mock

--- src/modules/QueryBuilder/__mocks__/getQueryJson.ts
+++ src/modules/QueryBuilder/__mocks__/getQueryJson.ts
@@ -1,21 +1,7 @@
-import { RuleGroupType } from "react-querybuilder";
+import { RuleGroupType } from "@/types/rules";
+import { EXAMPLE_1 } from "@/config/queryExamples";
 
 export const getQueryJson = (rest?: Partial<RuleGroupType>): RuleGroupType => ({
-  combinator: "and",
-  rules: [
-    {
-      field: "age",
-      operator: ">",
-      value: 60,
-      id: "82a1203f-9dc3-4b94-a473-afa5e33bc59a",
-    },
-    {
-      field: "condition",
-      operator: "=",
-      value: "201826",
-      id: "baf27009-46a0-49d9-acba-65b7111ef21d",
-    },
-  ],
-  id: "c15f7a8c-851b-4793-80ad-8ebde355c3bc",
+  ...EXAMPLE_1,
   ...rest,
 });
