0df77a244a89d46f573ccda128897baf660b4699
Calum Macdonald <calmacx@gmail.com>
2025-10-15 09:17:48 +0100

    move to stubs

--- src/actions/__mocks__/getCollections.ts
+++ src/actions/__mocks__/getCollections.ts
@@ -38,34 +38,32 @@ const getMockDemographics = ({
   ];
 };
 
-export const mockCollections: Collection[] = [
-  {
-    id: 1,
-    pid: "db6d9b451b818ccc9a449383f2f0c450",
-    name: "Test Dataset #1",
-    url: null,
-    type: QueryContext.BUNNY,
-    created_at: "2025-01-01 00:00:00",
-    updated_at: "2025-01-01 00:00:00",
-    demographics: getMockDemographics({ collection_id: 1, n: 1213 }),
-    size: getMockDistribution({ collection_id: 1, count: 1213 }),
-  },
-  {
-    id: 2,
-    pid: "db6d9b451b818ccc9a449383f2f0c451",
-    name: "Test Dataset #2",
-    url: null,
-    type: QueryContext.BUNNY,
-    created_at: "2025-01-01 00:00:00",
-    updated_at: "2025-01-01 00:00:00",
-    demographics: getMockDemographics({ collection_id: 2 }),
-    size: getMockDistribution({ collection_id: 1, count: 1213 }),
-  },
-];
+export const getMockCollection = (rest?: Partial<Collection>): Collection => ({
+  id: 1,
+  pid: "db6d9b451b818ccc9a449383f2f0c450",
+  name: "Test Dataset #1",
+  url: null,
+  type: QueryContext.BUNNY,
+  created_at: "2025-01-01 00:00:00",
+  updated_at: "2025-01-01 00:00:00",
+  demographics: getMockDemographics({ collection_id: 1, n: 1213 }),
+  size: getMockDistribution({ collection_id: 1, count: 1213 }),
+  ...rest,
+});
+
+export const getMockCollections = (n: number = 2): Collection[] =>
+  Array.from({ length: Math.max(0, n) }, (_, id) =>
+    getMockCollection({
+      id,
+      name: `Test Dataset #${id}`,
+      demographics: getMockDemographics({ collection_id: id }),
+      size: getMockDistribution({ collection_id: id, count: 1213 }),
+    })
+  );
 
 const getCollections = async (): Promise<ApiResponse<Collection[]>> => {
   return {
-    data: mockCollections,
+    data: getMockCollections(),
     message: "success",
   };
 };
