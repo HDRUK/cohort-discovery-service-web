a4d0ecc3aa4bef91fe61d5e0fc929dc4adbf18d1
Calum Macdonald <calmacx@gmail.com>
2025-10-15 08:51:13 +0100

    remore need for findBestMatch

--- src/utils/omop.ts
+++ src/utils/omop.ts
@@ -1,39 +1,5 @@
-import { Code, Option } from "@/types/api";
-
-const findBestMatch = (
-  value: string,
-  options: Option[]
-): Option | undefined => {
-  if (typeof value === "number") return undefined;
-  const lowerValue = value?.toLowerCase();
-  if (!lowerValue) return;
-
-  let bestScore = -Infinity;
-  let bestOption: Option | undefined;
-  if (!options) return;
-  for (const opt of options) {
-    const label = opt.label.toLowerCase();
-
-    let score = 0;
-    if (label === lowerValue) {
-      score = 100;
-    } else if (label.includes(lowerValue)) {
-      score = 80 - (label.indexOf(lowerValue) ?? 0);
-    } else {
-      const commonChars = [...lowerValue].filter((char) =>
-        label.includes(char)
-      ).length;
-      score = commonChars;
-    }
-
-    if (score > bestScore) {
-      bestScore = score;
-      bestOption = opt;
-    }
-  }
-
-  return bestOption;
-};
+import { Code } from "@/types/api";
+import { Option } from "@/types/common";
 
 const codesToOption = (codes: Code[]): Option[] =>
   codes
@@ -44,4 +10,4 @@ const codesToOption = (codes: Code[]): Option[] =>
       label: `${code.description} (${code.name}) `,
     }));
 
-export { codesToOption, findBestMatch };
+export { codesToOption };
