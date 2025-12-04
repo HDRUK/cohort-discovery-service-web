e722357019bc4d147bc172f46b962cc199dc0a75
Calum Macdonald <calmacx@gmail.com>
2025-12-04 10:25:24 +0000

    add in interval

--- src/config/defaults.ts
+++ src/config/defaults.ts
@@ -4,6 +4,7 @@ export const DEFAULT_USERS_PER_PAGE = 25;
 export const MAX_VARCHAR_LENGTH = 255;
 export const TRIGGER_GUTTER_PX = 70;
 export const ID_REF_SUFFIX = "hierarchyMenuItem";
+export const DEFAULT_INTERVAL = 1000;
 
 export const STATUS_LABELS: Record<string, string> = {
   ok: "Successful",
