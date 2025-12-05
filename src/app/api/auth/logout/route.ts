8d8236f5387bb97ebb081f942fae0a18bd42d871
Calum Macdonald <calmacx@gmail.com>
2025-12-05 12:37:45 +0000

    fix redirect

--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -1,14 +1,13 @@
 import { cookies } from "next/headers";
-import { NextResponse } from "next/server";
+import { redirect } from "next/navigation";
 import { GATEWAY_TOKEN_NAME } from "@/config/internals";
 
-export async function GET(req: Request) {
+export async function GET(_req: Request) {
   const cookieStore = await cookies();
   cookieStore.set(GATEWAY_TOKEN_NAME, "", {
     expires: new Date(0),
     path: "/",
   });
 
-  const url = new URL("/login", req.url);
-  return NextResponse.redirect(url);
+  redirect("/login");
 }
