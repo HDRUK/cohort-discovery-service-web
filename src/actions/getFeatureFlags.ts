0554a7082251f9ee8eb9523dd24fe2b63eee8e79
Calum Macdonald <calmacx@gmail.com>
2025-12-09 12:12:11 +0000

    fix revalidate

--- src/actions/getFeatureFlags.ts
+++ src/actions/getFeatureFlags.ts
@@ -1,14 +1,15 @@
 "use server";
 
+import { DEFAULT_REVALIDATE } from "@/config/defaults";
 import { apiGet } from "../lib/apiClient";
 import { API_ROUTES } from "../lib/apiRoutes";
 import { FeatureFlag, ApiResponse } from "../types/api";
 
 const getFeatureFlags = async (): Promise<ApiResponse<FeatureFlag>> => {
   return await apiGet<ApiResponse<FeatureFlag>>(API_ROUTES.featureFlags, {
-    next: { revalidate: 3600, tags: ["feature-flags"] },
+    next: { revalidate: DEFAULT_REVALIDATE, tags: ["feature-flags"] },
     cache: "force-cache",
   });
-}
+};
 
 export default getFeatureFlags;
