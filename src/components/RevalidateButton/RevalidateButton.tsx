6a0466749149d9498cfa3122b12035260f59afd6
Calum Macdonald <calmacx@gmail.com>
2025-12-19 13:31:36 +0000

    wrap for prod

--- src/components/RevalidateButton/RevalidateButton.tsx
+++ src/components/RevalidateButton/RevalidateButton.tsx
@@ -10,11 +10,25 @@ import {
 import RefreshIcon from "@mui/icons-material/Refresh";
 import { revalidateAction } from "@/actions/revalidate";
 
+const isProd = process.env.NODE_ENV === "production";
+
 export interface RevalidateButtonProps
   extends Omit<IconButtonProps, "onClick"> {
   tag: string;
   text?: string;
 }
+
+const TooltipWrapper = ({
+  enabled,
+  title,
+  children,
+}: {
+  enabled: boolean;
+  title: React.ReactNode;
+  children: React.ReactElement;
+}) => {
+  return enabled ? <Tooltip title={title}>{children}</Tooltip> : children;
+};
 
 export const RevalidateButton = ({
   tag,
@@ -22,13 +36,13 @@ export const RevalidateButton = ({
   ...rest
 }: RevalidateButtonProps) => {
   return (
-    <Tooltip title={tag}>
+    <TooltipWrapper enabled={!isProd} title={tag}>
       <Box display="flex" alignItems="center" gap={1}>
         <IconButton onClick={() => revalidateAction(tag)} {...rest}>
           <RefreshIcon fontSize={"small"} />
         </IconButton>
         {text && <Typography variant="body2">{text}</Typography>}
       </Box>
-    </Tooltip>
+    </TooltipWrapper>
   );
 };
