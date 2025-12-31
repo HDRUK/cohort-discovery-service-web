be0bb0dd72f2c2fc7807f714ac0cb7deb779cb6e
Calum Macdonald <calmacx@gmail.com>
2025-12-31 16:47:55 +0000

    add a full skeleton

---
+++ src/components/SkeletonFull/SkeletonFull.tsx
@@ -0,0 +1,7 @@
+import { Skeleton, SkeletonProps } from "@mui/material";
+
+const SkeletonFull = ({ sx }: { sx: SkeletonProps["sx"] }) => (
+  <Skeleton variant="rectangular" sx={{ flex: 1, minHeight: "100%", ...sx }} />
+);
+
+export default SkeletonFull;
