ab33b0ec9e723c21eb2962ae42bf63113b503574
Sam Cox <sam.cox@hdruk.ac.uk>
2026-01-06 10:35:25 +0000

    Remove new component for now

--- src/modules/UpdateCollections/index.ts
+++
@@ -1,5 +0,0 @@
-import UpdateCollections from "./UpdateCollections";
-import { UpdateCollectionsProps } from "./UpdateCollections";
-
-export type { UpdateCollectionsProps };
-export default UpdateCollections;
