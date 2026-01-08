61b1b69247e4f3a0ca7c73662aa9a7e15d0aa184
Vikas00230 <Vikas.Rachakonda@hdruk.ac.uk>
2026-01-08 16:40:27 +0000

    added files for semantic release

---
+++ release.config.js
@@ -0,0 +1,106 @@
+module.exports = {
+
+    branches: ["main"],
+
+    plugins: [
+
+      "@semantic-release/commit-analyzer",
+
+      [
+
+        "@semantic-release/release-notes-generator",
+
+        {
+
+          preset: "conventionalcommits",
+
+          presetConfig: {
+
+            types: [
+
+              { type: "feat", section: "✨ Features" },
+
+              { type: "fix", section: "🐛 Bug Fixes" },
+
+              { type: "perf", section: "⚡ Performance Improvements" },
+
+              { type: "docs", section: "📖 Documentation", hidden: false },
+
+              { type: "chore", section: "🔧 Maintenance", hidden: true },
+
+            ],
+
+            issuePrefixes: ["GAT", "TPD", "DP"], // Jira prefixes
+
+            issueUrlFormat: process.env.JIRA_URL + "{{prefix}}{{id}}"
+
+          }
+
+        }
+
+      ],
+
+      [
+
+        "@semantic-release/changelog",
+
+        {
+
+          changelogFile: "CHANGELOG.md",
+
+        }
+
+      ],
+
+      [
+
+        "@semantic-release/github",
+
+        {
+
+          successComment: false,
+
+          failComment: false,
+
+        }
+
+      ],
+
+      [
+
+        "@semantic-release/exec",
+
+        {
+
+          prepareCmd: "node updateVersions.js ${nextRelease.version} && git add chart/cohort-web/Chart.yaml package.json"
+
+        }
+
+      ],
+
+      [
+
+        "@semantic-release/git",
+
+        {
+
+          assets: [
+
+            "package.json",
+
+            "package-lock.json",
+
+            "CHANGELOG.md",
+
+            "chart/cohort-web/Chart.yaml"
+
+          ],
+          message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+          pushArgs: ["--force"]
+        }
+
+      ]
+
+    ]
+
+  };
