b8130b0b1de2f0867f26354140cf6ee2b758a029
Calum Macdonald <calmacx@gmail.com>
2026-01-13 11:27:02 +0000

    add another tag

--- src/actions/getUserCollections.ts
+++ src/actions/getUserCollections.ts
@@ -3,14 +3,14 @@
 import { apiGet, CachedGetArgs } from "../lib/apiClient";
 import { API_ROUTES } from "../lib/apiRoutes";
 import { Collection, ApiResponse } from "../types/api";
-import { TAG_COLLECTIONS_USER } from "@/config/tags";
+import { TAG_COLLECTIONS, TAG_COLLECTIONS_USER } from "@/config/tags";
 
 const getUserCollections = async (
   args?: Omit<CachedGetArgs, "url">
 ): Promise<ApiResponse<Collection[]>> => {
   return await apiGet<ApiResponse<Collection[]>>({
     url: API_ROUTES.userCollections,
-    tags: [TAG_COLLECTIONS_USER],
+    tags: [TAG_COLLECTIONS_USER, TAG_COLLECTIONS],
     ...args,
   });
 };
