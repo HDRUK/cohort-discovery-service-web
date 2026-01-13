3bee1e64d4e00ad61a441fc47fdd1518a939b317
Calum Macdonald <calmacx@gmail.com>
2026-01-13 11:38:24 +0000

    remove token utils

--- src/utils/token.ts
+++
@@ -1,5 +0,0 @@
-import { TokenUser } from "@/types/api";
-import { RoleName } from "@/types/roles";
-
-export const checkIsAdmin = (token_user: TokenUser | undefined | null) =>
-  token_user?.cohort_discovery_roles.includes(RoleName.ADMIN) || false;
