d8ab74a3ac0c7c37cefeaa6f07cd0a78df48ad50
Sam Cox <sam.cox@hdruk.ac.uk>
2026-01-27 09:44:30 +0000

    Lint

--- src/modules/CreateCollection/CreateCollection.tsx
+++ src/modules/CreateCollection/CreateCollection.tsx
@@ -1,4 +1,4 @@
-import { Box, Stack, Button, MenuItem, Typography, Link } from "@mui/material";
+import { Box, Stack, Button, MenuItem } from "@mui/material";
 import { useForm, Controller, FormProvider, useWatch } from "react-hook-form";
 import { CreateCollectionFormValues } from "@/types/forms";
 import { QueryContext } from "@/types/context";
