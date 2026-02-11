f1c59eece5e0cec984ba8e2273cc3e8e2f00061b
Calum Macdonald <calmacx@gmail.com>
2026-02-11 16:58:26 +0000

    fix

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -10,7 +10,6 @@ import getCustodians from "@/actions/getCustodians";
 import getFeatureFlags from "@/actions/getFeatureFlags";
 import { isStandalone } from "@/utils/modes";
 import { ErrorMode } from "@/lib/apiClient";
-import { Paper } from "@mui/material";
 
 const applicationMode = process.env.APPLICATION_MODE;
 
@@ -55,9 +54,6 @@ export default async function ProtectedLayout({
   const hasGeneralAccess = roles?.includes(RoleName.USER);
   const hasAdminAccess = roles.includes(RoleName.ADMIN);
   const hasTeamAccess = me.custodians.length > 0;
-
-  console.log({ roles, custodians: me.custodians });
-  console.log({ hasGeneralAccess, hasAdminAccess, hasTeamAccess });
 
   if (!(hasGeneralAccess || hasAdminAccess || hasTeamAccess)) {
     forbidden();
