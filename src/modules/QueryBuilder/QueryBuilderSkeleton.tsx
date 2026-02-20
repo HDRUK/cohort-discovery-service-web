3a08cf57a482c7ce27d25e43395143bb658adb61
Sam Cox <sam.cox@hdruk.ac.uk>
2026-02-20 12:59:39 +0000

    Escape quotes

--- src/modules/QueryBuilder/QueryBuilderSkeleton.tsx
+++ src/modules/QueryBuilder/QueryBuilderSkeleton.tsx
@@ -11,8 +11,8 @@ export const QueryBuilderSkeleton = () => (
   >
     <Typography textAlign="center" px={3} fontSize="large">
       Start by typing your query keywords in the Natural Language search box, or
-      build your query step-by-step using the buttons under 'Insert' in the
-      left-hand menu.
+      build your query step-by-step using the buttons under &apos;Insert&apos;
+      in the left-hand menu.
     </Typography>
   </Paper>
 );
