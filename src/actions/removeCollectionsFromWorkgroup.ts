6bc178710947398823be9141250ce0976ad5b092
Calum Macdonald <calmacx@gmail.com>
2026-02-20 13:46:38 +0000

    modify collection table

--- src/actions/removeCollectionsFromWorkgroup.ts
+++ src/actions/removeCollectionsFromWorkgroup.ts
@@ -2,7 +2,7 @@
 
 import { apiDelete } from "../lib/apiClient";
 import { API_ROUTES } from "../lib/apiRoutes";
-import { ApiResponse, RemoveCollectionsFromWorkgroupPost } from "../types/api";
+import { RemoveCollectionsFromWorkgroupPost, ApiResponse } from "../types/api";
 
 const removeCollectionsFromWorkgroup = async (
   payload: RemoveCollectionsFromWorkgroupPost,
