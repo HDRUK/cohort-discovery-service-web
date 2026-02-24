a5f6bf873f7cbc90fdee16f83de61d58bc34f61d
Calum Macdonald <calmacx@gmail.com>
2026-02-24 17:20:45 +0000

    more to search overlay

---
+++ src/components/CohortQueryInput/SearchOverlay.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import { Paper } from "@mui/material";
+import useQueryBuilder from "@/hooks/useQueryBuilder";
+import { Option } from "@/types/common";
+import Popper from "@mui/material/Popper";
+import List from "@/components/List";
+import { RuleGroupType } from "@/types/rules";
+import { Query } from "@/types/api";
+import { queryToText } from "@/utils/queryBuilder";
+
+type Props = {
+  queries: Query[];
+  options: (Option & { rules: RuleGroupType })[];
+  anchorEl: HTMLElement | null;
+  open: boolean;
+};
+const SearchOverlay = ({ queries, options, anchorEl, open }: Props) => {
+  const setQueryBuilderJson = useQueryBuilder((qb) => qb.setQueryBuilderJson);
+
+  return (
+    <Popper
+      open={open}
+      anchorEl={anchorEl}
+      placement="bottom-start"
+      modifiers={[{ name: "offset", options: { offset: [0, 4] } }]}
+      style={{ zIndex: 2000 }}
+    >
+      <Paper
+        sx={{
+          width: anchorEl?.clientWidth,
+          boxShadow: 3,
+          borderRadius: 1,
+          p: 1,
+        }}
+      >
+        <List
+          items={[
+            {
+              label: "Query Examples",
+              items: options.map((opt) => ({
+                label: opt.label,
+                value: opt.rules.id,
+                onClick: () => setQueryBuilderJson(opt.rules),
+              })),
+            },
+            {
+              label: "Recent Searches",
+              items: queries.map((q) => ({
+                id: q.id,
+                label: queryToText(q.definition),
+                value: q.name,
+                onClick: () => setQueryBuilderJson(q.definition),
+              })),
+            },
+          ]}
+        />
+      </Paper>
+    </Popper>
+  );
+};
+
+export default SearchOverlay;
