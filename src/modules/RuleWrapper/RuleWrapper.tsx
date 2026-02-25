6d4fc52cfecfa91441acb13675641f6f69c6f101
Sam Cox <sam.cox@hdruk.ac.uk>
2026-02-25 12:58:23 +0000

    Add memo deps

--- src/modules/RuleWrapper/RuleWrapper.tsx
+++ src/modules/RuleWrapper/RuleWrapper.tsx
@@ -161,7 +161,7 @@ const RuleWrapper = ({
         });
       }
     },
-    [id, toggleSelected, isSelected],
+    [id, toggleSelected, isSelected, node, select, deselect],
   );
 
   const onMouseOver = (e: React.MouseEvent<HTMLDivElement>) => {
