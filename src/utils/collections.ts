112af8d8582b8f6923c42da3b13acd4e19c16610
Calum Macdonald <calmacx@gmail.com>
2026-03-19 14:16:04 +0000

    remove dup

--- src/utils/collections.ts
+++ src/utils/collections.ts
@@ -1,14 +1,5 @@
 import { Collection } from "@/types/api";
 
-const getSelectableDatasetIds = (
-  collections: Collection[],
-  includeSynthetic: boolean,
-) => {
-  return collections
-    .filter((c) => includeSynthetic || !c.is_synthetic)
-    .map((c) => c.pid);
-};
-
 const getAllowedDatasetIds = (
   collections: Collection[],
   includeSynthetic: boolean,
@@ -18,4 +9,4 @@ const getAllowedDatasetIds = (
     .map((c) => c.pid);
 };
 
-export { getAllowedDatasetIds, getSelectableDatasetIds };
+export { getAllowedDatasetIds };
