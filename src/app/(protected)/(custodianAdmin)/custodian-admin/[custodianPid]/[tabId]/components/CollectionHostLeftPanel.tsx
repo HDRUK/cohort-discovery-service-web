a8fb3eb60b03525ed3bcfd7caeec8b429005413d
Calum Macdonald <calmacx@gmail.com>
2026-04-17 11:40:22 +0100

    implement manage menu

--- src/app/(protected)/(custodianAdmin)/custodian-admin/[custodianPid]/[tabId]/components/CollectionHostLeftPanel.tsx
+++ src/app/(protected)/(custodianAdmin)/custodian-admin/[custodianPid]/[tabId]/components/CollectionHostLeftPanel.tsx
@@ -4,6 +4,8 @@ import ActionMenuSection from "@/components/ActionMenuSection";
 import AddButton from "@/components/AddButton";
 import CreateCollectionHost from "@/modules/CreateCollectionHost";
 import { useThreePane } from "@/providers/ThreePaneProvider";
+import List from "@/components/List";
+import useSearchParams from "@/hooks/useSearchParams";
 
 type CollectionHostLeftPanelProps = {
   custodianId: number;
@@ -13,6 +15,11 @@ const CollectionHostLeftPanel = ({
   custodianId,
 }: CollectionHostLeftPanelProps) => {
   const { expandedLeft, toggleLeft } = useThreePane();
+
+  //does nothing yet
+  // - here for the future if a host filter is to be implemented
+  const { getSearchParam } = useSearchParams("collection_host_filter");
+  const searchParam = getSearchParam();
 
   return (
     <Box
@@ -51,6 +58,23 @@ const CollectionHostLeftPanel = ({
           />
         </ActionMenuSection>
       )}
+
+      <ActionMenuSection
+        hidden={expandedLeft}
+        title={"Manage"}
+        defaultExpanded
+        underline
+      >
+        <List
+          items={[
+            {
+              label: "All Hosts",
+              onClick: () => ({}),
+              selected: !searchParam,
+            },
+          ]}
+        />
+      </ActionMenuSection>
     </Box>
   );
 };
