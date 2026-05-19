4d90faac26d08939097a88a19111a44f18b07ec2
Calum Macdonald <calmacx@gmail.com>
2026-05-19 13:56:16 +0100

    remove logs

--- src/actions/regressionTest/deleteRegressionTest.ts
+++ src/actions/regressionTest/deleteRegressionTest.ts
@@ -1,20 +1,14 @@
 "use server";
 
-import { apiDelete, ErrorMode } from "@/lib/apiClient";
+import { apiDelete } from "@/lib/apiClient";
 import { API_ROUTES } from "@/lib/apiRoutes";
 import { ApiResponse } from "@/types/api";
 import { revalidateUserAction } from "@/actions/revalidate";
 import { TAG_REGRESSION_TESTS } from "@/config/tags";
 
-const deleteRegressionTest = async (pid: string): Promise<ApiResponse<[]>> => {
-  const res = await apiDelete<ApiResponse<[]>>(API_ROUTES.regressionTest(pid), {
-    cache: "no-store",
-    errorMode: ErrorMode.RESULT,
-  });
-  console.log(API_ROUTES.regressionTest(pid));
-  console.log({ res });
+const deleteRegressionTest = async (pid: string) => {
+  await apiDelete<ApiResponse<[]>>(API_ROUTES.regressionTest(pid));
   await revalidateUserAction(TAG_REGRESSION_TESTS);
-  return res;
 };
 
 export default deleteRegressionTest;
