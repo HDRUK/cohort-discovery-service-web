57a5cb2199937e408da360338489d2de247a7efe
SimeonTu <45809363+SimeonTu@users.noreply.github.com>
2026-06-09 09:55:27 +0100

    enable collection hosts table sorting (#408)

--- src/actions/collectionHost/getCustodianCollectionHosts.ts
+++ src/actions/collectionHost/getCustodianCollectionHosts.ts
@@ -1,16 +1,18 @@
 "use server";
 
 import { getCollectionHostTag, TAG_COLLECTION_HOSTS } from "@/config/tags";
-import { apiGet } from "@/lib/apiClient";
+import { apiGet, CachedGetArgs } from "@/lib/apiClient";
 import { API_ROUTES } from "@/lib/apiRoutes";
 import { ApiResponse, CollectionHost } from "@/types/api";
 
 const getCustodianCollectionHosts = async (
   custodianPid: string,
+  args?: Omit<CachedGetArgs, "url">,
 ): Promise<ApiResponse<CollectionHost[]>> => {
   return await apiGet<ApiResponse<CollectionHost[]>>({
     url: API_ROUTES.custodianCollectionHosts(custodianPid),
     tags: [TAG_COLLECTION_HOSTS, getCollectionHostTag(custodianPid)],
+    ...args,
   });
 };
 
