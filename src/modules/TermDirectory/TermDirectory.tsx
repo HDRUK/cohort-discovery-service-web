214a791804f5510bed659a0bcba13018140f8d82
SimeonTu <45809363+SimeonTu@users.noreply.github.com>
2026-08-11 16:50:57 +0100

    temporarily disable sorting

--- src/modules/TermDirectory/TermDirectory.tsx
+++ src/modules/TermDirectory/TermDirectory.tsx
@@ -74,6 +74,7 @@ const TermDirectory = ({
     perPageDefault: DEFAULT_PER_PAGE,
     getRowId: (row) => String(row?.concept_id),
     enableStickyHeader: true,
+    enableSorting: false,
   });
 
   return (
