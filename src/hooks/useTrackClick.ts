72dd71ddd5c1c34c25ee3a71263d0de0530c494f
Calum Macdonald <calmacx@gmail.com>
2026-08-24 17:27:54 +0100

    remove line comment

--- src/hooks/useTrackClick.ts
+++ src/hooks/useTrackClick.ts
@@ -3,7 +3,6 @@
 import { useCallback } from "react";
 import trackClick, { TrackClickInput } from "@/actions/trackClick";
 
-// Fire-and-forget click tracking; never throws so a failed track can't break the UI.
 const useTrackClick = () => {
   return useCallback((input: TrackClickInput) => {
     trackClick(input).catch(() => {});
