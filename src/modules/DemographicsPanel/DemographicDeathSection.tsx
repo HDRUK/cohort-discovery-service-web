8da7108e9d4f996ad7f83704eb1d6c3b6758cd3b
SimeonTu <45809363+SimeonTu@users.noreply.github.com>
2026-08-31 16:47:46 +0100

    update selection design to "toggle"

--- src/modules/DemographicsPanel/DemographicDeathSection.tsx
+++ src/modules/DemographicsPanel/DemographicDeathSection.tsx
@@ -11,12 +11,15 @@ import * as React from "react";
 import ToggleButton from "@mui/material/ToggleButton";
 import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
 import { useQueryBuilderStore } from "@/store/queryBuilderStore";
+import { useState } from "react";
 
 const DemographicLocationSection = (props: DemographicRowActionProps) => {
   const { control } = useFormContext<Demographics>();
   const { death } = useQueryBuilder((qb) => ({
     death: qb.queryBuilderJson.demographics?.death ?? null,
   }));
+
+  const [active, setActive] = useState(false);
 
   const { location } = useQueryBuilder((qb) => ({
     location: qb.queryBuilderJson.demographics?.location ?? null,
@@ -37,6 +40,8 @@ const DemographicLocationSection = (props: DemographicRowActionProps) => {
     </ToggleButton>,
   ];
 
+  const size = 24;
+
   useQueryBuilderStore((qb) => {
     console.log(qb);
   });
@@ -54,21 +59,119 @@ const DemographicLocationSection = (props: DemographicRowActionProps) => {
           <Controller
             name="death"
             control={control}
-            render={({ field }) => (
-              <>
-                <ToggleButtonGroup
-                  value={field.value}
-                  onChange={(_e, val) => field.onChange(val)}
-                  aria-label="Death options"
-                  exclusive
-                >
-                  {children}
-                </ToggleButtonGroup>
-                <Typography variant="body2" color="text.secondary">
-                  {note}
-                </Typography>
-              </>
-            )}
+            render={({ field }) => {
+              console.log("death field: ", field);
+
+              return (
+                <>
+                  <Box
+                    sx={{
+                      position: "relative",
+                      display: "inline-flex",
+                      alignItems: "center",
+                      width: "fit-content",
+                      maxWidth: "fit-content",
+
+                      "&::after": {
+                        content: '""',
+                        position: "absolute",
+                        left: size / 2,
+                        right: size / 2,
+                        top: "50%",
+                        transform: "translateY(-50%)",
+                        height: size * 0.5,
+                        bgcolor: "grey.400",
+                        zIndex: 0,
+                        pointerEvents: "none",
+                      },
+                    }}
+                  >
+                    <Stack direction={"row"} gap={0.3}>
+                      <Box
+                        data-testid="toggle-action-off"
+                        onClick={(e) => {
+                          field.onChange(e.currentTarget.innerText);
+                        }}
+                        sx={{
+                          width: "fit-content",
+                          height: size,
+                          minWidth: size,
+                          minHeight: size,
+                          borderRadius: size,
+                          bgcolor:
+                            field.value === "Unknown/Alive"
+                              ? "white"
+                              : "grey.400",
+                          border: field.value === "Unknown/Alive" ? 1 : 0,
+                          cursor: "pointer",
+                          display: "flex",
+                          alignItems: "center",
+                          justifyContent: "center",
+                          px: 2,
+                          zIndex: 1,
+                          "& svg": {
+                            transition: "transform 0.15s ease",
+                          },
+
+                          "&:hover svg": {
+                            transform: "scale(1.15)",
+                          },
+                        }}
+                      >
+                        <Typography
+                          variant="body2"
+                          color={
+                            field.value === "Unknown/Alive" ? "green" : "white"
+                          }
+                        >
+                          Unknown/Alive
+                        </Typography>
+                      </Box>
+                      <Box
+                        data-testid="toggle-action-on"
+                        onClick={(e) =>
+                          field.onChange(e.currentTarget.innerText)
+                        }
+                        sx={{
+                          width: "fit-content",
+                          height: size,
+                          minWidth: size,
+                          minHeight: size,
+                          borderRadius: size,
+                          bgcolor:
+                            field.value === "Death recorded"
+                              ? "white"
+                              : "grey.400",
+                          border: field.value === "Death recorded" ? 1 : 0,
+                          cursor: "pointer",
+                          display: "flex",
+                          alignItems: "center",
+                          justifyContent: "center",
+                          px: 2,
+                          zIndex: 1,
+                          "& svg": {
+                            transition: "transform 0.15s ease",
+                          },
+
+                          "&:hover svg": {
+                            transform: "scale(1.15)",
+                          },
+                        }}
+                      >
+                        <Typography
+                          variant="body2"
+                          color={
+                            field.value === "Death recorded" ? "green" : "white"
+                          }
+                        >
+                          Death recorded
+                        </Typography>
+                      </Box>
+                    </Stack>
+                  </Box>
+                </>
+              );
+            }}
           />
         </Box>
       }
