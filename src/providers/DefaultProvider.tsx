577dc7bafe8cb9b9fc1c7ebe168aa805174c2734
Calum Macdonald <calmacx@gmail.com>
2026-09-07 14:19:34 +0100

    resolve merge issue

--- src/providers/DefaultProvider.tsx
+++ src/providers/DefaultProvider.tsx
@@ -16,14 +16,11 @@ export type DefaultConfig = {
   searchPrefetch: number;
   searchWaitTime: number;
   searchSuggestionRotation: number;
-<<<<<<< HEAD
   pingAWarnMs: number;
   pingAFailMs: number;
   pingBWarnMs: number;
   pingBFailMs: number;
-=======
   locationMinRadius: number;
->>>>>>> origin/dev
   serviceDeskUrl: string;
   serviceDeskSupportSuffix: string;
   serviceDeskReportBugSuffix: string;
@@ -51,14 +48,11 @@ export const DefaultProvider = ({
       searchPrefetch: defaults.DEFAULT_SEARCH_PREFETCH,
       searchWaitTime: defaults.DEFAULT_SEARCH_WAIT_TIME,
       searchSuggestionRotation: defaults.DEFAULT_SEARCH_SUGGESTION_ROTATION,
-<<<<<<< HEAD
       pingAWarnMs: defaults.DEFAULT_PING_A_WARN_MS,
       pingAFailMs: defaults.DEFAULT_PING_A_FAIL_MS,
       pingBWarnMs: defaults.DEFAULT_PING_B_WARN_MS,
       pingBFailMs: defaults.DEFAULT_PING_B_FAIL_MS,
-=======
       locationMinRadius: MIN_RADIUS,
->>>>>>> origin/dev
       serviceDeskUrl: "",
       serviceDeskSupportSuffix: "",
       serviceDeskReportBugSuffix: "",
