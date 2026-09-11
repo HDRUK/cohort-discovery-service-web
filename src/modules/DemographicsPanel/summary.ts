71a22244199d49934bd5353ac3ac038a24232635
SimeonTu <45809363+SimeonTu@users.noreply.github.com>
2026-09-11 16:09:37 +0100

    add comments

--- src/modules/DemographicsPanel/summary.ts
+++ src/modules/DemographicsPanel/summary.ts
@@ -15,7 +15,13 @@ export const formatLocationSummary = (
 
 export const formatDeathSummary = (death: Option | null): string => {
   return death
-    ? `Death ${death?.value === 0 ? DEATH_OPTIONS[0].label.toLowerCase() : DEATH_OPTIONS[1].label.toLowerCase()}`
+    ? `Death ${
+        death?.value === 0
+          ? // No death recorded
+            DEATH_OPTIONS[0].label.toLowerCase()
+          : // Death recorded
+            DEATH_OPTIONS[1].label.toLowerCase()
+      }`
     : "Death Any";
 };
 
