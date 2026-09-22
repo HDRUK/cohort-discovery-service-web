24e131cf8402ba3070eca9d852acf1f8dfe17140
SimeonTu <45809363+SimeonTu@users.noreply.github.com>
2026-09-22 15:22:20 +0100

    bring back second row

--- src/modules/Rule/RuleSearch.tsx
+++ src/modules/Rule/RuleSearch.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Concept } from "@/types/api";
-import { Box, Button, Stack, Typography } from "@mui/material";
+import { Box, Button, Divider, Stack, Typography } from "@mui/material";
 import { useCallback, useEffect, useMemo, useState } from "react";
 import { useForm, useWatch } from "react-hook-form";
 import SearchConcepts from "@/components/SearchConcepts";
@@ -130,34 +130,6 @@ const RuleSearch = ({ onConfirm, isSelected, onSelect }: RuleSearchProps) => {
     </Stack>
   ) : null;
 
-  const confirmButtons = isMultiSelect && hasOptions && (
-    <Stack direction="row" justifyContent="flex-end" gap={1} pt={1}>
-      <Button
-        variant="outlined"
-        color="secondary"
-        size="small"
-        onClick={(e) => {
-          e.stopPropagation();
-          clearAll();
-        }}
-      >
-        Clear selection
-      </Button>
-      <Button
-        variant="contained"
-        color="secondary"
-        size="small"
-        disabled={selectedConcepts.length < 1}
-        onClick={(e) => {
-          e.stopPropagation();
-          handleConfirm();
-        }}
-      >
-        Confirm selection
-      </Button>
-    </Stack>
-  );
-
   return (
     <Box
       data-testid="rule-search-container"
@@ -178,8 +150,37 @@ const RuleSearch = ({ onConfirm, isSelected, onSelect }: RuleSearchProps) => {
         // If we find that multi-select works fine, then need to delete this,
         // and other related single-select code in the future.
         // headerSlot={toggleRow}
-        confirmSlot={confirmButtons}
       />
+      {isMultiSelect && hasOptions && (
+        <>
+          <Divider sx={{ mt: 1 }} />
+          <Stack direction="row" justifyContent="flex-end" gap={1} pt={1}>
+            <Button
+              variant="outlined"
+              color="secondary"
+              size="small"
+              onClick={(e) => {
+                e.stopPropagation();
+                clearAll();
+              }}
+            >
+              Cancel
+            </Button>
+            <Button
+              variant="contained"
+              color="secondary"
+              size="small"
+              disabled={selectedConcepts.length < 1}
+              onClick={(e) => {
+                e.stopPropagation();
+                handleConfirm();
+              }}
+            >
+              Confirm selection
+            </Button>
+          </Stack>
+        </>
+      )}
     </Box>
   );
 };
