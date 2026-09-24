a3a790a61a6e167c06452c6320fcf255d1ee11c5
SimeonTu <45809363+SimeonTu@users.noreply.github.com>
2026-09-24 11:15:03 +0100

    add scroll when clicking on hierarchy items

--- src/components/HierarchyItem/HierarchyItem.tsx
+++ src/components/HierarchyItem/HierarchyItem.tsx
@@ -17,6 +17,8 @@ import useNodeActions from "@/hooks/useNodeActions";
 import { trueKeys } from "@/utils/numbers";
 import SquareRadio from "../SquareRadio";
 import useHoverable from "@/hooks/useHoverable";
+import { useCohortBuilderContext } from "@/providers/CohortBuilderProvider";
+import { useCallback } from "react";
 
 type HierarchyItemProps = {
   node: RuleNodeType;
@@ -70,6 +72,8 @@ export const HierarchyItem = ({
 
   const isGroup = isRuleGroup(node) && node.rules.length > 0;
 
+  const { scrollToNode } = useCohortBuilderContext();
+
   const toggleCheckbox = (e: React.MouseEvent<HTMLElement>) => {
     e.preventDefault();
     e.stopPropagation();
@@ -79,6 +83,7 @@ export const HierarchyItem = ({
 
     const nextParent = !selected[node.id];
     toggleSelected(node.id, !isShift);
+    scrollToNode(node.id);
     if (!isRuleGroup(node)) return;
 
     node.rules.forEach((r) => {
